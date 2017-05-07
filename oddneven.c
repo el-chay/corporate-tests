f7d1a91c8a18fe138be6ee44075859582c579be8
Antonio Chay <achay@pindrop.com>
2017-05-06 23:27:10 -0400

    now with parameters.

--- oddneven.c
+++ oddneven.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mkswap.h"
 MKSWAP(int)
 
-int main()
+int main(int argc, char *argv[])
 {
-    int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    int *b = a, *e = a+9;
+    int len = argc-1, *a = malloc(len*sizeof(int));
+    for(int i = 1; i < argc; i++)
+        a[i-1] = atoi(argv[i]);
+
+    int *b = a, *e = a+len-1;
     do{
         while(*b%2) b++;
         while(!(*e%2)) e--;
     } while(e > b && swap(b++, e--));
 
-    for(int i=0; i<10; i++) printf("[%d]:%d\n", i, a[i]);
+    for(int i=0; i<len; i++) printf("[%d]:%d\n", i, a[i]);
     return 0;
 }
