723b16dd9f175d3a8e1aa2f375adcb5db2f477d6
Antonio Chay <achay@pindrop.com>
2017-05-07 10:22:38 -0400

    various changes

--- mkswap.h
+++ mkswap.h
@@ -1,2 +1,10 @@
+/* Make-a-Swap Macro.
+ * The macro will produce a swap function with the requested type.
+ * The function will be local to the file/unit.
+ * It will return the value contained in the first parameter.
+ * Example:
+ * MKSWAP(char) will generate:
+ * static char swap(char *a, char *b) { ... return *a; }
+ */
 #define MKSWAP(T) \
 static T swap(T *a, T *b) { T tmp = *a; *a = *b; *b = tmp; return *a; }
