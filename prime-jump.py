457fbf2490504ecae173af1e893b937b7ed825bd
Antonio Chay <antonio@chay.cl>
2021-02-04 21:45:05 -0500

    Final

--- prime-jump.py
+++ prime-jump.py
@@ -4,28 +4,30 @@ from math import sqrt, pow
 def maxGameScore(cell):
     max_len = len(cell)+1
     # Get primes
-    erastos = [True for i in range(0, max_len)]
+    eratos = [True for i in range(0, max_len)]
     for i in range(2, int(sqrt(max_len))+1):
-        if erastos[i]:
-            c = 0; j = int(pow(i,2))+c*i
+        if eratos[i]:
+            c = 0
+            j = int(pow(i,2))
             while j < max_len:
-                erastos[j] = False
-                c = c+1
+                eratos[j] = False
+                c += 1
                 j = int(pow(i,2))+c*i
+
+    # Select jumps (primes ending in 3)
     i = 2
     jumps = [1,]
     while i < max_len:
-        if erastos[i] and i%10 == 3:
+        if eratos[i] and i%10 == 3:
             jumps.append(i)
         i += 1
 
+    # Get the greater-than path
     i = 0
     sum = cell[0]
-    while i < len(cell):
+    possible_jumps = [(cell[i+j], i+j) for j in jumps if i+j < len(cell)]
+    while i < len(cell) and len(possible_jumps) > 0:
+        max_value, i = sorted(possible_jumps, key=lambda x: x[0])[-1]
+        sum += max_value
         possible_jumps = [(cell[i+j], i+j) for j in jumps if i+j < len(cell)]
-        if len(possible_jumps) > 0:
-            max_value, i = sorted(possible_jumps, key=lambda x: x[0], reverse=True)[0]
-            sum += max_value
-        else:
-            break
     return sum
